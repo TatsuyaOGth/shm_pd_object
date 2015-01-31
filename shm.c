43968774968be2a322a3965e73d4dbf8595ca901
tatsuyaogusu <tatsuyaogusu@gmail.com>
2015-02-01 01:51:32 +0900

    update...

--- shm.c
+++ shm.c
@@ -12,9 +12,6 @@
 
 static t_class * shm_class;
 
-static const char   DEF_MEMORY_KEY[MAXPDSTRING] = "pd_shm";
-static const int    DEF_MEMORY_SIZE = 256;
-
 typedef struct _shm
 {
     t_object x_obj;
@@ -25,7 +22,7 @@ typedef struct _shm
     unsigned int    memorySize;
     char            memoryKey[MAXPDSTRING];
     char            dataType[MAXPDSTRING];
-    void            * sharedData;
+    void            *sharedData;
     
 } t_shm;
 
@@ -34,7 +31,7 @@ static char *shm_version = "$vertion: 0.0.1 $";
 static void shm_key(t_shm *x, t_symbol *s);     /// set memory key
 static void shm_size(t_shm *x, t_float f);      /// set memory size
 static void shm_get(t_shm *x, t_symbol *s, int argc, t_atom *argv);    /// get share data as type
-static void shm_connect(t_shm *x);              /// try connect to shared memory
+static void shm_connect(t_shm *x, t_symbol *s, int argc, t_atom *argv); /// try connect to shared memory
 static void shm_disconnect(t_shm *x);           /// disconnect
 static void shm_print(t_shm *x);                /// dump infomation
 
@@ -45,7 +42,7 @@ static void *shm_new(t_symbol *s, int argc, t_atom *argv)
     int i;
     
     t_shm *x = (t_shm *)pd_new(shm_class);
-//    outlet_new(&x->x_obj, &s_list);
+    outlet_new(&x->x_obj, &s_list);
     x->x_outlet = outlet_new(&x->x_obj, &s_bang);
     
     // init
@@ -53,34 +50,10 @@ static void *shm_new(t_symbol *s, int argc, t_atom *argv)
     x->isReady = 0;
     x->isServer = 0;
     x->sharedData = NULL;
-    for (i = 0; i < MAXPDSTRING; ++i)
-    {
-        x->memoryKey[i] = 0;
-        x->dataType[i] = 0;
-    }
     
     if (argc > 0)
     {
-        // get arguments
-        while (argc--)
-        {
-            switch (argv->a_type)
-            {
-                case A_FLOAT: shm_size(x, atom_getfloat(argv)); break;
-                case A_SYMBOL: shm_key(x, atom_getsymbol(argv)); break;
-                default: logpost(x, 1, "[shm] err: Unsupported atom type"); break;
-            }
-            argv++;
-        }
-        if (x->memorySize > 0)
-        {
-            shm_connect(x);
-        }
-    }
-    else {
-        // set default
-        strcpy(x->memoryKey, DEF_MEMORY_KEY);
-        x->memorySize = DEF_MEMORY_SIZE;
+        shm_connect(x, s, argc, argv);
     }
     
     return (x);
@@ -98,39 +71,81 @@ static void shm_bang(t_shm *x)
 {
     if (x->isReady != 0 && x->sharedData != NULL)
     {
-//        post("%f", (float *)x->sharedData);
         outlet_float(x->x_outlet, *(float *)x->sharedData);
     }
-//    post("bang %d", x->isReady);
+    else logpost(x, 1, "[shm] err: still not connected");
 }
 
 static void shm_key(t_shm *x, t_symbol *s)
 {
     strcpy(x->memoryKey, s->s_name);
-    logpost(x, 2, "[shm] notice: set new memory key = %s", x->memoryKey);
+//    logpost(x, 2, "[shm] notice: set new memory key = %s", x->memoryKey);
 }
 
 static void shm_size(t_shm *x, t_float f)
 {
     x->memorySize = floor(f);
-    logpost(x, 2, "[shm] notice: set new memory size = %d", x->memorySize);
+//    logpost(x, 2, "[shm] notice: set new memory size = %d", x->memorySize);
 }
+
 
 static void shm_get(t_shm *x, t_symbol *s, int argc, t_atom *argv)
 {
+    if (x->isReady != 0 && x->sharedData != NULL)
+    {
+        logpost(x, 1, "[shm] err: still not connected");
+        return;
+    }
+    if (argc % 2 != 0)
+    {
+        logpost(x, 1, "[shm] err: [type] [num] ...");
+        return;
+    }
+    
+    void * data = x->sharedData;
+    t_atom *outv = (t_atom *)malloc(sizeof(t_atom) * 64);
+    int *type = (int *)malloc(sizeof(int) * 64);
+    int num = 0;
+    
     if (argc > 0)
     {
         while (argc--)
         {
-            switch (argv->a_type)
+            if (argc % 2 == 0)
             {
-                case A_FLOAT: shm_size(x, atom_getfloat(argv)); break;
-                case A_SYMBOL: shm_key(x, atom_getsymbol(argv)); break;
-                default: logpost(x, 1, "[shm] err: Unsupported atom type"); break;
+                char c = *(atom_getsymbol(argv)->s_name);
+                switch (c)
+                {
+                    case 'f': *type = 0; break;
+                    case 's': *type = 1; break;
+                    default: logpost(x, 1, "[shm] err: unknown type"); break;
+                }
+                ++type;
+                ++num;
             }
+            else if (argc % 2 == 1)
+            {
+                switch (*type)
+                {
+                    case 0:
+                        
+                        break;
+                }
+            }
+//            switch (argv->a_type)
+//            {
+//                case A_FLOAT: post("check %f", atom_getfloat(argv)); break;
+//                case A_SYMBOL: post("check %s", atom_getsymbol(argv)->s_name); break;
+//            }
             argv++;
+            
         }
     }
+        
+    outlet_list(x->x_obj.ob_outlet, s, num, outv);
+    
+    free(outv);
+    free(type);
 }
 
 static void shm_print(t_shm *x)
@@ -139,13 +154,59 @@ static void shm_print(t_shm *x)
     logpost(x, 2, "[shm] notice: memory size = %d", x->memorySize);
 }
 
-static void shm_connect(t_shm *x)
+static void shm_connect(t_shm *x, t_symbol *s, int argc, t_atom *argv)
 {
     int i;
     int sum;
     int fd;
     
     x->isReady = 0;
+    
+    // set key and size
+    if (argc > 0)
+    {
+        // while (argc--)
+        // {
+        //     switch (argv->a_type)
+        //     {
+        //         case A_FLOAT: shm_size(x, atom_getfloat(argv)); break;
+        //         case A_SYMBOL: shm_key(x, atom_getsymbol(argv)); break;
+        //         default: logpost(x, 1, "[shm] err: Unsupported atom type"); break;
+        //     }
+        //     argv++;
+        // }
+        for (i = 0; i < argc; ++i)
+        {
+            if (i == 0)
+            {
+                if (argv->a_type == A_SYMBOL)
+                {
+                    shm_key(x, atom_getsymbol(argv));
+                }
+                else logpost(x, 1, "[shm] err: arg 1 unsupported atom type");
+            }
+            if (i == 1)
+            {
+                if (argv->a_type == A_FLOAT)
+                {
+                    shm_size(x, atom_getint(argv));
+                }
+                else logpost(x, 1, "[shm] err: arg 2 unsupported atom type");
+            }
+            if (i == 2)
+            {
+                if (argv->a_type == A_FLOAT)
+                {
+                    x->isServer = (atom_getfloat(argv) != 0);
+                }
+                else logpost(x, 1, "[shm] err: arg 3 unsupported atom type");
+            }
+        }
+    }
+    else {
+        logpost(x, 1, "[shm] err: empty argument");
+        return;
+    }
     
     // check key and size
     if (x->memorySize == 0)
@@ -170,7 +231,7 @@ static void shm_connect(t_shm *x)
         x->sharedData = mmap(0, x->memorySize, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
         if (x->sharedData != NULL) {
             x->isReady = 1;
-            logpost(x, 2, "[shm] notice: connection succeed key: %s, size: %d", x->memoryKey, x->memorySize);
+            logpost(x, 2, "[shm] notice: connection succeed [key %s][size %d][server %d]", x->memoryKey, x->memorySize, x->isServer);
         }
         else {
             logpost(x, 1, "[shm] err: failed connect to shared memory");
@@ -193,26 +254,6 @@ static void shm_disconnect(t_shm *x)
     }
 }
 
-static void shm_any(t_shm *x, t_symbol *s, int argc, t_atom *argv)
-{
-    if (s != NULL) post("%c", s);
-    if (x->isReady != 0)
-    {
-        while(argc--)
-        {
-            post("%f", atom_getfloat(argv));
-            argv++;
-        }
-    }
-    else logpost(x, 1, "[shm] err: still not connected");
-}
-
-//static void shm_list(t_shm *x, t_symbol *s, int argc, t_atom *argv)
-//{
-//    shm_any(x, 0, argc, argv);
-//}
-
-
 
 //---------------------------------------------------------- setup
 
@@ -225,14 +266,12 @@ void shm_setup(void)
                                CLASS_DEFAULT, A_GIMME, 0);
     
     class_addbang(shm_class, shm_bang);
-    class_addmethod(shm_class, (t_method)shm_key, gensym("key"), A_DEFSYMBOL, 0);
-    class_addmethod(shm_class, (t_method)shm_size, gensym("size"), A_FLOAT, 0);
+//    class_addmethod(shm_class, (t_method)shm_key, gensym("key"), A_DEFSYMBOL, 0);
+//    class_addmethod(shm_class, (t_method)shm_size, gensym("size"), A_FLOAT, 0);
     class_addmethod(shm_class, (t_method)shm_get, gensym("get"), A_GIMME, 0);
     class_addmethod(shm_class, (t_method)shm_print, gensym("print"), 0);
     class_addmethod(shm_class, (t_method)shm_connect, gensym("connect"), A_GIMME, 0);
     class_addmethod(shm_class, (t_method)shm_disconnect, gensym("disconnect"), 0);
-//    class_addlist(shm_class, shm_list);
-//    class_addanything(shm_class, shm_any);
     
     logpost(NULL, 2, "[shm] %s, author: Tatsuya Ogusu", shm_version);
     logpost(NULL, 2, "[shm] is still in development, tcompiled against Pd version %d.%d.%d",PD_MAJOR_VERSION, PD_MINOR_VERSION, PD_BUGFIX_VERSION);
